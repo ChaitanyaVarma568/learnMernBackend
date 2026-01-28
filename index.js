fdf236267a875288477e3970fcd62ed4361c7574
Chaitanya <chaitanyavarma568@gmail.com>
2026-01-28 18:18:59 +0530

    Added qr image generator

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-import x from "@inquirer/prompts";
+import { input } from "@inquirer/prompts";
+import qr from "qr-image";
+import fs from "fs";
 
-console.log(x);
+const answer = await input({ message: "Enter the QR link text!" });
+
+let qr_svg = qr.image(answer, { type: "png" });
+qr_svg.pipe(fs.createWriteStream("qr.png"));
