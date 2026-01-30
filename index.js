4f1106e28b750a1496d5167e802102da8cff8f78
Chaitanya Varma <chaitanyavarma568@gmail.com>
2026-01-30 11:40:48 +0530

    Installed Express js Framework

--- index.js
+++
@@ -1,8 +0,0 @@
-import { input } from "@inquirer/prompts";
-import qr from "qr-image";
-import fs from "fs";
-
-const answer = await input({ message: "Enter the QR link text!" });
-
-let qr_svg = qr.image(answer, { type: "png" });
-qr_svg.pipe(fs.createWriteStream(`${answer}.png`));
